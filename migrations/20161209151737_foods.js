5a0653e2ae8c0bcf2d20cab70f9abf1403cf912a
Evan Busse <evanbusse@gmail.com>
2016-12-10 13:20:08 -0700

    edited food schema

--- migrations/20161209151737_foods.js
+++ migrations/20161209151737_foods.js
@@ -6,6 +6,8 @@ exports.up = function(knex, Promise) {
     table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').index();
     table.string('image_url').defaultTo('https://goo.gl/CWVE6o');
     table.text('comments').notNullable().defaultTo('');
+    table.boolean('is_active').defaultTo(true);
+    table.integer('category').defaultTo(1).references('id').inTable('categories');
     table.timestamps(true, true);
   });
 };
