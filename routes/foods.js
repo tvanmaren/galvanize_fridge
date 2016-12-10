5dded844b8cf708c95dd70e6e1baf3ea4f300712
Evan Busse <evanbusse@gmail.com>
2016-12-10 13:29:59 -0700

    user can get items in the fridge

--- routes/foods.js
+++ routes/foods.js
@@ -4,18 +4,33 @@
 
 const express = require('express');
 const ev = require('express-validation');
-// const validations = require('../validations/books');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
-
 const knex = require('../knex');
-
-const {
-  camelizeKeys,
-  decamelizeKeys
-} = require('humps');
-
+const {camelizeKeys,decamelizeKeys} = require('humps');
 const boom = require('boom');
+
+router.get('/active', (req, res, next) => {
+  knex('food')
+  .where('active', true)
+  .then((data) => {
+    if(data.length === 0) {
+      res.status(416).send('Requested Range not satisfiable');
+    }
+    res.send(data);
+  });
+});
+
+router.get('/active/:filter', (req, res, next) => {
+  var activeItems;
+
+  knex('food')
+  .where('active', true)
+  .then((data) => {
+    activeItems = data;
+  })
+  .then(() => {
+    
+  });
+});
 
 module.exports = router;
