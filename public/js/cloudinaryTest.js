fbfccf9179a1910d79378885e6a04cb29f6577fb
Kevin Cowley <cowley.kevin.l@gmail.com>
2016-12-10 14:35:31 -0700

    JWT sign/verify code added, still need to store token

--- public/js/cloudinaryTest.js
+++ public/js/cloudinaryTest.js
@@ -8,15 +8,16 @@ $(function() {
   });
 });
 
-function uploadPhoto() {
-  console.log("clicked button");
-  cloudinary.openUploadWidget({ cloud_name: 'dgt2xab7d', upload_preset: 'x2hiolgr'}, 
-      function(error, result) {
-        console.log(error, result);
-      });
-  // $('#upload_field').unsigned_cloudinary_upload("x2hiolgr", function() {
-  //   console.log("THIS IS A CALLBACK");
-  // });
+// function uploadPhoto() {
+//   console.log("clicked button");
+//   cloudinary.openUploadWidget({ cloud_name: 'dgt2xab7d', upload_preset: 'x2hiolgr'},
+//       function(error, result) {
+//         console.log(error, result);
+//       });
+  console.log($('#upload_field:file'));
+  $('#upload_field').unsigned_cloudinary_upload("x2hiolgr", function() {
+    console.log("THIS IS A CALLBACK");
+  });
   // $('#upload_field').append($.cloudinary.unsigned_upload_tag("x2hiolgr",
   // { cloud_name: 'demo' }, function() {
   //   console.log("HELLOOOO");
