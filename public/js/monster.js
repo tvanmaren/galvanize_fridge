fb3f4c78c7e62eab17466587b2c173e6d57e906e
Anna Lotko <annaklotko@gmail.com>
2016-12-12 16:29:35 -0700

    face is loaded on fridge page with flash animation

---
+++ public/js/monster.js
@@ -0,0 +1,35 @@
+'use strict';
+$(function() {
+  console.log("loaded");
+  AJAX_JSON_Req(jsonfile);
+});
+
+var jsonfile = "fridge_monster.json",
+fps = 24,
+width = 800,
+height = 300,
+AJAX_req;
+
+// TODO: This should be called on a delay
+
+
+function handle_AJAX_Complete() {
+  if( AJAX_req.readyState == 4 && AJAX_req.status == 200)
+  {
+    var container = document.getElementById("svg_container");
+    var json = JSON.parse(AJAX_req.responseText);
+    var comp = new SVGAnim(json, width,height,fps);
+    container.appendChild(comp.s.node);
+  }
+
+}
+
+function AJAX_JSON_Req( url )
+{
+    AJAX_req = new XMLHttpRequest();
+    AJAX_req.open("GET", url, true);
+    AJAX_req.setRequestHeader("Content-type", "application/json");
+
+    AJAX_req.onreadystatechange = handle_AJAX_Complete;
+    AJAX_req.send();
+}
