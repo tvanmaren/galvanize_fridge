ebe8fad98053090beaa3b4d431822f08da805654
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-12-13 12:28:15 -0700

    :bug: fix modal opening too soon with blank data

--- public/js/fridge.js
+++ public/js/fridge.js
@@ -1,78 +1,78 @@
 'use strict';
 
-$(function() {
+$(function () {
 
-    let admin;
+  let admin;
 
-    //check if I'm an admin, and assign that to a global
-    $.getJSON('/users/self/')
-        .done((user) => {
-            admin = user.isAdmin;
-        })
-        .fail((err) => {
-            console.error(err);
-        });
-
-    // Click on Settings Icon
-    $('#settings').click(function() {
-        if (admin) {
-            checkFridgeStats();
-        } else {
-            checkUserInfo();
-        }
+  //check if I'm an admin, and assign that to a global
+  $.getJSON('/users/self/')
+    .done((user) => {
+      admin = user.isAdmin;
+    })
+    .fail((err) => {
+      console.error(err);
     });
 
-    var foodsJSON;
-    //generate cards on page load
-    $.getJSON('/foods')
-        .done((data) => {
-            foodsJSON = data;
-            generateCards(data);
-        })
-        .fail((err) => {
-            console.error(err);
-        });
+  // Click on Settings Icon
+  $('#settings').click(function () {
+    if (admin) {
+      checkFridgeStats();
+    } else {
+      checkUserInfo();
+    }
+  });
 
-    //Radio Button Listeners (Sorting fridge cards);
-    $('#allCat').click(function() {
-        $('#foodCards').empty();
-        generateCards(foodsJSON);
+  var foodsJSON;
+  //generate cards on page load
+  $.getJSON('/foods')
+    .done((data) => {
+      foodsJSON = data;
+      generateCards(data);
+    })
+    .fail((err) => {
+      console.error(err);
     });
 
-    $('#personalCat').click(function() {
-        $('#foodCards').empty();
-        generateCards(foodsJSON.filter((obj) => {
-            return obj.category === 1;
-        }));
-    });
+  //Radio Button Listeners (Sorting fridge cards);
+  $('#allCat').click(function () {
+    $('#foodCards').empty();
+    generateCards(foodsJSON);
+  });
 
-    $('#communityCat').click(function() {
-        $('#foodCards').empty();
-        generateCards(foodsJSON.filter((obj) => {
-            return obj.category === 2;
-        }));
-    });
+  $('#personalCat').click(function () {
+    $('#foodCards').empty();
+    generateCards(foodsJSON.filter((obj) => {
+      return obj.category === 1;
+    }));
+  });
 
-    $('#eventCat').click(function() {
-        $('#foodCards').empty();
-        generateCards(foodsJSON.filter((obj) => {
-            return obj.category === 3;
-        }));
-    });
+  $('#communityCat').click(function () {
+    $('#foodCards').empty();
+    generateCards(foodsJSON.filter((obj) => {
+      return obj.category === 2;
+    }));
+  });
 
-    // LOGOUT
-    $('.logout').click(function() {
-        logout();
-    });
+  $('#eventCat').click(function () {
+    $('#foodCards').empty();
+    generateCards(foodsJSON.filter((obj) => {
+      return obj.category === 3;
+    }));
+  });
+
+  // LOGOUT
+  $('.logout').click(function () {
+    logout();
+  });
 });
 
 function generateCards(jsonObject) {
-    var $foodDiv = $('#foodCards');
+  var $foodDiv = $('#foodCards');
 
-    jsonObject.map((obj) => {
-        var categoryName = setCategory(obj.category);
+  jsonObject.map((obj) => {
+    var categoryName = setCategory(obj.category);
 
-        var newCard = `
+    var newCard = `
       <div class="col s12 m6 l4">
       <div class="card">
       <div class="card-image">
@@ -90,93 +90,97 @@ function generateCards(jsonObject) {
       </div>
       `;
 
-        $foodDiv.append(newCard);
+    $foodDiv.append(newCard);
 
-        var Id = `#${obj.id}`;
+    var Id = `#${obj.id}`;
 
-        $(Id).click(function() {
-            console.log($(this).attr('id'));
-            deleteItem($(this).attr('id'));
-        });
+    $(Id).click(function () {
+      console.log($(this).attr('id'));
+      deleteItem($(this).attr('id'));
     });
+  });
 }
 
 function setCategory(catID) {
-    switch (catID) {
-        case 1:
-            return 'Personal';
-        case 2:
-            return 'Community';
-        case 3:
-            return 'Event';
-        default:
-            return 'Personal';
-    }
+  switch (catID) {
+  case 1:
+    return 'Personal';
+  case 2:
+    return 'Community';
+  case 3:
+    return 'Event';
+  default:
+    return 'Personal';
+  }
 }
 
 function deleteItem(id) {
-    $.ajax({
-        url: `/foods/${id}`,
-        type: "DELETE",
-        success: function(result) {
-            location.reload();
-            console.log("Delete successful " + result);
-        }
-    });
+  $.ajax({
+    url: `/foods/${id}`,
+    type: "DELETE",
+    success: function (result) {
+      location.reload();
+      console.log("Delete successful " + result);
+    }
+  });
 }
 
 function checkFridgeStats() {
   $('#name').text(`Fridge History`);
-    $.getJSON("/users")
-        .then((userList) => {
-            userList.forEach((user) => {
-                $.getJSON(`/foods/${user.id}/`)
-                    .then((result) => {
-                            $('#content').html(`${$('#content').html()} <p> ${user.firstName} ${user.lastName} has ${result.length} items in the fridge.`);
-                        },
-                        (err) => {
-                            console.error(err);
-                        });
+  $.getJSON("/users")
+    .then((userList) => {
+      $('#content').empty();
+      userList.forEach((user) => {
+        $.getJSON(`/foods/${user.id}/`)
+          .then((result) => {
+              $('#content').append(`<p> ${user.firstName} ${user.lastName} (${user.email}) has ${result.length} items in the fridge.`);
+            },
+            (err) => {
+              console.error(err);
             });
-        });
+      });
+      $('#adminPanel').modal('open');
+    });
 
-    $.getJSON("/foods")
-        .then((result) => {
-            //TODO add user data (items per user) & expiration data
-            console.log(result);
-            $('#content').html(`${$('#content').html()} <p> Fridge items to date: ${result.length}`);
-        }, (err) => {
-            console.error(err);
-        });
+  $.getJSON("/foods")
+    .then((result) => {
+      //TODO add user data (items per user) & expiration data
+      console.log(result);
+      $('#content').append(`<p> Fridge items to date: ${result.length}`);
+    },
+    (err) => {
+      console.error(err);
+    });
 }
 
 function checkUserInfo() {
-    var $xhr = $.ajax({
-        type: "GET",
-        url: "/users/self/",
-        success: function(result) {
-            $('#name').text(`${result.firstName} ${result.lastName}`);
-            $('#content').text(`Email: ${result.email}`);
-            $('#content').append(`<p> User ID: ${result.id}`);
-            console.log("GET successful ", result);
-        }
-    });
+  var $xhr = $.ajax({
+    type: "GET",
+    url: "/users/self/",
+    success: function (result) {
+      $('#content').empty();
+      $('#name').text(`${result.firstName} ${result.lastName}`);
+      $('#content').text(`Email: ${result.email}`);
+      $('#content').append(`<p> User ID: ${result.id}`);
+      $('#adminPanel').modal('open');
+    }
+  });
 
-    $xhr.fail((err) => {
-        console.error(err);
-    });
+  $xhr.fail((err) => {
+    console.error(err);
+  });
 }
 
 function logout() {
-    var $xhr = $.ajax({
-        type: "DELETE",
-        url: "/token",
-        success: function(result) {
-            console.log("DELETE /token successful ", result);
-        }
-    });
+  var $xhr = $.ajax({
+    type: "DELETE",
+    url: "/token",
+    success: function (result) {
+      console.log("DELETE /token successful ", result);
+    }
+  });
 
-    $xhr.fail((err) => {
-        console.error(err);
-    });
+  $xhr.fail((err) => {
+    console.error(err);
+  });
 }
