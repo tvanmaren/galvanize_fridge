e2b0077ed29c08661970833a2aa505ba4ce9ee8a
Cassie Peterson <cas.j.peters@gmail.com>
2016-12-13 17:04:28 -0700

    add working ticker

--- public/js/announcementTicker.js
+++ public/js/announcementTicker.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
   console.log("getting to announcementTicker.js");
+  $('.newsticker').newsTicker();
 })
+
+$('.newsticker').newsTicker({
+    row_height: 20,
+    max_rows: 1,
+    speed: 600,
+    direction: 'up',
+    duration: 4000,
+    autostart: 1,
+    pauseOnHover: 0
+});
