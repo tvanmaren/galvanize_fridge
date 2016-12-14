385bb1f422aa7323c706ee418109b18e9780602a
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-12-14 15:45:22 -0700

    :sparkles: three new announcements

--- seeds/04_announcements.js
+++ seeds/04_announcements.js
@@ -12,9 +12,19 @@ exports.seed = function(knex, Promise) {
           content: 'There is leftover food from a meet-up in the kitchen'
         }),
         knex('announcements').insert({
-          user_id: 1,
+          user_id: 2,
           title: 'Missing Tupperware',
           content: 'My favorite tupperware is missing! Let me know if you find it'
+        }),
+        knex('announcements').insert({
+          user_id: 1,
+          title: 'Missing Tupperware',
+          content: 'My favorite tupperware is missing, too! Let\'s be friends!'
+        }),
+        knex('announcements').insert({
+          user_id: 5,
+          title: 'Missing Tupperware',
+          content: 'I found a sandwich on the ground... slack me if it\'s yours?'
         })
       ]);
     })
